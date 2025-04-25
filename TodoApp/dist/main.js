9a3e6b3d45c6b00cc16a3fb3a84e02e7cff7dee7
Diego Pasten <diegopasten78@gmail.com>
2025-04-24 23:49:56 -0400

    feat: :zap: Main general

--- TodoApp/dist/main.js
+++ TodoApp/dist/main.js
@@ -1,31 +1,23 @@
 import { TaskService } from "./services/TaskService.js";
-
 // Instanciar servicio y cargar tareas guardadas
 const service = new TaskService();
 service.loadFromLocalStorage();
-
 // Referencias a elementos del DOM
-const form = document.getElementById("task-form") as HTMLFormElement;
-const input = document.getElementById("task-input") as HTMLFormElement;
-const taskList = document.getElementById("task-list") as HTMLFormElement;
-
+const form = document.getElementById("task-form");
+const input = document.getElementById("task-input");
+const taskList = document.getElementById("task-list");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-
     const taskText = input.value.trim();
-
-    if (taskText === "") return;
-
+    if (taskText === "")
+        return;
     const li = document.createElement("li");
     li.textContent = taskText;
-
     //botón eleiminar
     const btn = document.createElement("button");
     btn.textContent = "🗑️";
     btn.onclick = () => li.remove();
-
     li.appendChild(btn);
     taskList.appendChild(li);
     input.value = "";
-})
-
+});
